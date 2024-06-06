ad2134ca2c80bbe058193c001cf1c39e4ac12eb8
ArthPatel-GitHub <221008@trinityschools.nz>
2024-06-07 10:03:07 +1200

    I HAVE UPDATED THE INSTRUCTIONS AGAIN AND HAVE JUST DONE A DOUBLE CHECK IF THE CODE IS RUNNING PROPERLY SO THE QUIZES WORK PROPERLY. AND THEY ARE, SO I BELIEVE I HAVE FINISHED MY CODE TO THE BEST OF MY ABILITY.

--- INTERNAL.py
+++ INTERNAL.py
@@ -2,13 +2,16 @@ import random
 
 # Instructions for the quizzes
 INSTRUCTIONS = """
-This is a game that has three quizzes. What you need to do is enter a number for which quiz level 
-you would like to do, then the quiz you have selected will start. You will 
-answer the questions that the GAME gives you, with your score being tallied at the end. If you 
-would like to see your quiz history, please enter '5' on your keyboard. If you 
-would like to exit this quiz, please enter '6'. We highly recommend you have a calculator for our quizzes because
-we are known for making the most difficult quizzes!!!!! If you like to leave the quiz, please enter the letter 'c', because we
-don't like letters in maths! Only Numbers!!! Please make sure to do your best!!! Because we love maths!!!!!!
+This is a game that has three quizzes. What you need to do is enter a number (2 - 4)
+for which quiz level you would like to do, then the quiz you have selected will start. You
+will then be asked how many questions you would like to answer (please enter a positive integer
+or 'i' for infinite amount of questions) and then the quiz will start! If you would like to exit 
+the quiz while answering the questions, please enter 'c' on the question you are on and the quiz will finish.
+At the end of the quiz, your score will be tallied and shown to you. If you would like too see the full results
+you must play at least one quiz, then enter the number '5' on the menu to see the history. If you would like to
+leave our awesome quiz. Please enter '6' on the menu. We strongly recommend that you use a calculator for our
+quizes, because we are known for having the most difficult quizes on the planet! (Not really, but they are 
+challenging!) And thats all there is too it! Please have fun and as always, do your best!!!!!
 
 Note: Please round your answers to 1 decimal place if necessary.
 """
