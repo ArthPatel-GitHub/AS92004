eda07456e149c8e134f7200e70caca476bd61785
ArthPatel-GitHub <221008@trinityschools.nz>
2024-06-11 13:01:37 +1200

    I HAVE MADE A NEW FILE ON THE INTERNAL FILE BECAUSE THE ONE THAT SAYS "INTERNAL WITH GUI" I HAVE MADE IT WITH A GUI AND THE ONE THAT JUST SAYS "INTERNAL" JUST RUNS OF THE VS CODE.

--- INTERNAL.py
+++ INTERNAL.py
@@ -1,24 +1,6 @@
 import random
 
-# Instructions for the quizzes, I have used a constant here called "INSTRUCTIONS" because I want this
-# just to be for the instructions without it changing, so that is why I have made is a constant.
-INSTRUCTIONS = """
-This is a game that has three quizzes. What you need to do is enter a number (2 - 4)
-for which quiz level you would like to do, then the quiz you have selected will start. You
-will then be asked how many questions you would like to answer (please enter a positive integer
-or 'i' for infinite amount of questions) and then the quiz will start! If you would like to exit 
-the quiz while answering the questions, please enter 'c' on the question you are on and the quiz will finish.
-At the end of the quiz, your score will be tallied and shown to you. If you would like too see the full results
-you must play at least one quiz, then enter the number '5' on the menu to see the history. If you would like to
-leave our awesome quiz. Please enter '6' on the menu. We strongly recommend that you use a calculator for our
-quizzes, because we are known for having the most difficult quizzes on the planet! (Not really, but they are 
-challenging!) And thats all there is too it! Please have fun and as always, do your best!!!!!
-
-Note: Please round your answers to 1 decimal place if necessary.
-"""
-
-# Function to generate primary math questions, what this function does is it generates the questions for when
-#the user chooses the primary math quiz so they can have generated questions with the infinite mode.
+# 
 def generate_primary_question():
     num1 = random.randint(1, 10)
     num2 = random.randint(1, 5)
